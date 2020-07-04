5f16a152e1898da9419016e4345807064ad71b1f
pequenojoohn <kheymk@hotmail.com>
2020-07-03 21:04:31 -0300

    First register user.

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,20 @@
+import User from '../models/User';
+
+class UserController {
+    async store(request, response) {
+        const userExists = await User.findOne({
+            where: {
+                email: request.body.email,
+            },
+        });
+
+        if (userExists) {
+            return response.status(400).json({ error: 'User already exist!' });
+        }
+
+        const { id, name, email, provider } = await User.create(request.body);
+        return response.json({ id, name, email, provider });
+    }
+}
+
+export default new UserController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (request, response) => {
-    const user = await User.create({
-        name: 'Jonathan Matos',
-        email: 'jonathan.matos2445@gmail.com',
-        password_hash: '123456789',
-    });
-    response.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
