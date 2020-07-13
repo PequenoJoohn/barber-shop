05d12a8fe5222444913e209b9fde3e9578bf9db7
pequenojoohn <kheymk@hotmail.com>
2020-07-13 20:19:35 -0300

    Configuration Mail, using Mailtrap.

---
+++ backend/src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '02296a72eb124d',
+        pass: 'f35905b1d61cab',
+    },
+    default: {
+        from: 'Jonathan Matos <jonathan.matos2445@gmail.com>',
+    },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+
+// Mailtrap (DEV)
