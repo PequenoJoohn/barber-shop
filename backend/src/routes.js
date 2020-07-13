b639c3ea05c75927cdcce7325fc3b86b600eac50
pequenojoohn <kheymk@hotmail.com>
2020-07-13 15:53:20 -0300

    Create Schedule of date.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
+import ScheduleControllers from './app/controllers/ScheduleControllers';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,6 +25,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleControllers.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
